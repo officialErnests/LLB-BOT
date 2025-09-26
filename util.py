ce91897a0e1b2f9b35d8fa0d9661fa52bc561fbd
n0o0b090lv <ernestspursw@gmail.com>
2025-09-26 23:54:51 +0300

    Cleaner code :)) as well red ball detection

--- util.py
+++ util.py
@@ -12,11 +12,12 @@ class window_cut:
 
 class llb_bot:
     windows = None
-    img_test = cv.imread('testimg/test4.png',cv.IMREAD_UNCHANGED)
+    img_test = cv.imread('testimg/test3.png',cv.IMREAD_UNCHANGED)
     img_ball = cv.imread('examples/ball/Ball.png',cv.IMREAD_UNCHANGED)
     w, h = img_ball.shape[1::-1]
     coolRect = None
     window_set = False
+    ball_state = 0
     def __init__(self, windowName):
         if len(pygetwindow.getWindowsWithTitle(windowName)):
             self.windows = pygetwindow.getWindowsWithTitle(windowName)[0]
@@ -44,20 +45,13 @@ class llb_bot:
             else:
                 img_hsv_value = cv.cvtColor(self.img_test, cv.COLOR_BGR2HSV)
                 start_img = self.img_test
-            #209, 95, 100
-            #Why does it have 0-179
-            #Also why is it default GBR
-            #at the end i spend like 3min manualy homing in :))
-            mask_upper = np.array([105, 244, 255])
-            mask_lower = np.array([105, 243, 255])
-            masked_screenshot = cv.inRange(img_hsv_value, mask_lower, mask_upper)
-            # time.sleep(0.1)
-            # cv.findContours(masked_screenshot, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE, countours)
-            # print(contours)
-            # cv.rectangle(masked_screenshot,contours, bottom_right, 255, 2)
-            # print(funny_touple[0])
-            # cv.drawContours(masked_screenshot, contours, 0, (0, 255, 0), 2)
-            ret,thresh = cv.threshold(masked_screenshot,127,255,0)
+            
+            #BLUE BALL
+            mask_upper_blue = np.array([105, 244, 255])
+            mask_lower_blue = np.array([105, 243, 255])
+            masked_screenshot_blue = cv.inRange(img_hsv_value, mask_lower_blue, mask_upper_blue)
+
+            ret,thresh = cv.threshold(masked_screenshot_blue,254,255,0)
             movement = cv.moments(thresh)
             if movement['m00'] > 0:
                 cX = int(movement["m10"] / movement["m00"])
@@ -66,6 +60,24 @@ class llb_bot:
                 cv.circle(start_img, (cX, cY), 10, (0, 0, 0), -1)
                 cv.circle(start_img, (cX, cY), 5, (255, 255, 0), -1)
                 cv.putText(start_img, "x:" + str(cX) + " y:"+ str(cX), (100,100), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), 4)
+            ret,thresh = cv.threshold(masked_screenshot_blue,254,255,0)
+
+            #RED BALL
+            mask_upper_red = np.array([5, 230, 255])
+            mask_lower_red = np.array([5, 229, 255])
+            masked_screenshot_red = cv.inRange(img_hsv_value, mask_lower_red, mask_upper_red)
+            
+            ret,thresh = cv.threshold(masked_screenshot_red,254,255,0)
+            movement = cv.moments(thresh)
+            if movement['m00'] > 0:
+                cX = int(movement["m10"] / movement["m00"])
+                cY = int(movement["m01"] / movement["m00"])
+                cv.circle(start_img, (cX, cY), 20, (255, 255, 255), -1)
+                cv.circle(start_img, (cX, cY), 10, (0, 0, 0), -1)
+                cv.circle(start_img, (cX, cY), 5, (0, 0, 255), -1)
+                cv.putText(start_img, "x:" + str(cX) + " y:"+ str(cX), (100,100), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 4)
+            
+            
             cv.imshow('funn.',start_img)
             if cv.waitKey(1) == ord('q'):
                 cv.destroyAllWindows()

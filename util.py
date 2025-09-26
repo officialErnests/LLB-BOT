921085746eff54838b900102f54ea209dc1caf5d
n0o0b090lv <ernestspursw@gmail.com>
2025-09-26 17:00:46 +0300

    Ball is up and installed auto gui :))

--- util.py
+++ util.py
@@ -1,6 +1,16 @@
-import cv2
+import cv2 as cv
 
 def test():
-    img_test = cv2.imread('testimg/test4',0)
-    cv2.imshow('graycsale image',img_test)
-    cv2.waitKey(0)
+    img_test = cv.imread('testimg/test2.png',cv.IMREAD_UNCHANGED)
+    img_ball = cv.imread('examples/ball/Ball.png',cv.IMREAD_UNCHANGED)
+    w, h = img_ball.shape[1::-1]
+    # 0c88ff
+ 
+    result = cv.matchTemplate(img_test, img_ball, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(result,top_left, bottom_right, 255, 2)
+    cv.imshow('graycsale image',result)
+    cv.waitKey(0)

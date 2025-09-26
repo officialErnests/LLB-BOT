0cf5129cab9dc6e6ad68b6f4627b061c2d1d6235
n0o0b090lv <ernestspursw@gmail.com>
2025-09-26 17:32:11 +0300

    THX WINDOWCAPTURE :DD Now will have to modify it a bit ;))

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LLB-BOT
 Leathal league blaze bot using opencv :))
+
+# Py scripts
+(thanks to him i can have decent fps :DD)
+THXXX/windowcapture.py - https://github.com/learncodebygaming/opencv_tutorials?tab=MIT-1-ov-file
--- util.py
+++ util.py
@@ -1,16 +1,25 @@
 import cv2 as cv
+import pyautogui 
+import numpy as np
+from THXXX.windowcapture import WindowCapture
+
+wincap = WindowCapture('LLBlaze')
 
 def test():
     img_test = cv.imread('testimg/test2.png',cv.IMREAD_UNCHANGED)
     img_ball = cv.imread('examples/ball/Ball.png',cv.IMREAD_UNCHANGED)
     w, h = img_ball.shape[1::-1]
-    # 0c88ff
- 
-    result = cv.matchTemplate(img_test, img_ball, cv.TM_CCOEFF_NORMED)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(result,top_left, bottom_right, 255, 2)
-    cv.imshow('graycsale image',result)
-    cv.waitKey(0)
+
+    while (True):
+        screenshot = wincap.get_screenshot()
+        open_cv_image = np.array(screenshot)
+        # result = cv.matchTemplate(open_cv_image, img_ball, cv.TM_CCOEFF_NORMED)
+        # min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+        # # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+        # top_left = max_loc
+        # bottom_right = (top_left[0] + w, top_left[1] + h)
+        # cv.rectangle(result,top_left, bottom_right, 255, 2)
+        cv.imshow('funn.',open_cv_image)
+        if cv.waitKey(1) == ord('q'):
+            cv.destroyAllWindows()
+            break

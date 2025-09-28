182d7ee3683effc62cbf367789b5cb6094267ce6
n0o0b090lv <ernestspursw@gmail.com>
2025-09-28 14:19:15 +0300

    Finnished wall tracing :DD

--- Real_utils.py
+++ Real_utils.py
@@ -23,13 +23,9 @@ class vector2D():
     def __mul__(self, mul):
         return vector2D(self.x * mul, self.y * mul)
     def distance_till_intersection(self, borders, direction_vec):
-        #hate math ;-;
-        #did this on paper, can check #noob-dev for it ;-;
         x_distance = borders[0] - self.x if direction_vec.x < 0 else -1 if direction_vec.x == 0 else borders[2] - self.x
         y_distance = borders[1] - self.y if direction_vec.y < 0 else -1 if direction_vec.y == 0 else borders[3] - self.y
-        # print(self, borders, x_distance, y_distance)
-        
-        #handles edge cases (so div by zero XD)
+
         if direction_vec.x == 0 and direction_vec.y == 0:
             return 0
         if direction_vec.x == 0:
@@ -41,17 +37,11 @@ class vector2D():
         ratio_y = y_distance / direction_vec.y
 
         if ratio_x < ratio_y:
-            # print(x_distance, y_distance, ratio_x, ratio_y, direction_vec.x, direction_vec.y)
             y_distance = (direction_vec.y * x_distance) / direction_vec.x
-            # print(x_distance, y_distance)
-            # return x_distance
             return vector2D(x_distance, y_distance).magnitude()
-        return 0
-        if x_distance < y_distance:
-            print(x_distance, y_distance, direction_vec.y, direction_vec.x, vector2D(x_distance, direction_vec.y / direction_vec.x * x_distance).len())
-            return vector2D(x_distance, direction_vec.y / direction_vec.x * x_distance).len()
         else:
-            return vector2D(direction_vec.x /  direction_vec.y * y_distance, y_distance).len()
+            x_distance = (direction_vec.x * y_distance) / direction_vec.y
+            return vector2D(x_distance, y_distance).magnitude()
     def magnitude(self):
         return abs(self.x) + abs(self.y)
     def len(self):
